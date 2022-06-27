b635189a66d87f1da833055ffa53262bca70fee9
Adam Wagh <adamwagh@gmail.com>
2022-06-26 20:02:00 -0500

    I added selector criteria, verification checks, arrays and concat arrays. I am trying to get the random element selector to work for my array

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ This is a password generator I built for challenge 3 during the UT Austin coding
 I claim no ownership over the HTML, CSS or lines 1-14 of the JavaScript code. This was provided as part of the assignment and did not alter it in any way. 
 
 Alert message - https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_alert
+
+JavaScript array concat: https://www.w3schools.com/jsref/jsref_concat_array.asp
+
+empty array = https://www.quora.com/How-do-you-declare-an-empty-array-in-JavaScript
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,6 +2,7 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -13,68 +14,114 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
- // The following section contains the code that controls the prompts that will determine the parameters of the password. Console.logs are there to verify this code is working and should be commented out prior to submitting project //
+ // This function determines the password length.
+    var length = ""
 
-    let pwdLength = prompt("How many characters would you like (min:8 , max:128)")
-    console.log(pwdLength)
-
-    // Trying to figure out how to reload the page immediately if the number falls outside acceptable parameters so the user doesnt go through the rest of the questions to have the page reload on them.// 
-
+    var passwordLengthFunction = function () {
+        let pwdLength = prompt("How many characters would you like (min:8 , max:128)")
+    
+      
         if (pwdLength < 8) {
-            alert("You need at least 8 characters, reload the page and try again");
-            window.stop();
-            location.reload();
+                  alert("You need at least 8 characters, reload the page and try again");
+                  passwordLengthFunction()
         }
+        else if (pwdLength > 128) {
+            alert ('That is over the maximum number of characters, try again')
+            passwordLengthFunction()
+        }
+        else {
+            length = pwdLength
+        }
+      }
+      // runs function 
+      passwordLengthFunction();
 
-        // } else if (pwdLength > 128) {
-        //     alert("Nobody needs a password that big, refresh and try again!");
-        //     location.reload();
-        // } else {
-        //     alert("you want a pasword with " + pwdLength + " characters? No problemo!");
-        // }
+      //logs output of length to the console. Should be removed later 
+      console.log ("The password length should be: " + length + " characters long!")
+
+    // The following prompts will set the remaining criteria for the password. console logs are included now so I can check my work, but will probably be removed once the program runs properly
 
     let lowerCase = confirm('Do you want lower case letters')
-    console.log (lowerCase)
+    console.log ('The password should contain lower case letters: ' + lowerCase)
 
     let upperCase = confirm ('Do you want upper case letters')
-    console.log(upperCase)
+    console.log('The password should contain upper case letters : ' + upperCase)
 
     let numbers = confirm ('Would you like numbers in the password?')
-    console.log (numbers)
+    console.log ("The password should contain numbers : " + numbers)
 
     let specialCharacters = confirm ('Do you want to use any special characters')
-    console.log (specialCharacters)
+    console.log ("The password should contain specail characters : " + specialCharacters)
 
-    // The following section contains variables to store the answers from the prompt section. Again the console.logs should be commented out prior to submitting the project//
+    // This if statement validates that the password will contain at least one of the designated password elements are present. If none of the elements are selected it will refresh the page and force the user to start the process over again //
 
-    var length = pwdLength
-    console.log ('The password length should be ' + length)
-
-    var lCase = lowerCase
-    console.log ('Should the password have lower case letters? : ' + lCase)
-
-    var uCase= upperCase
-    console.log ('Should the password have upper case letters? : ' + uCase)
-
-    var number = numbers
-    console.log ('Should there be numbers in the password? : ' + number)
-
-    var specialChar = specialCharacters
-    console.log ('Should the password have special characters? : ' + specialChar)
+    if (lowerCase == false && upperCase == false && numbers == false && specialCharacters == false) {
+        alert ('You must select at least 1 character type to continue'); 
+        location.reload();
+    }
 
 // The following are the arrays for uppercase, lower case, numbers and special characters// 
     var uc = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-    console.log (uc)
+    // console.log (uc)
 
     var lc = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-    console.log (lc)
+    // console.log (lc)
 
     var num = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-    console.log(num)
+    // console.log(num)
 
     var sc = ["~", "`", "!", "@", "#", "$", "%", "^", "&","*", "(", ")", "-", "_", "+", "=", "{", "}", "[", "]","|", "/", ":", ";", "'", '"', "<", ">", ",", ".", "?",'\\']
-    console.log (sc)
- 
+    // console.log (sc)
+
+    // This section builds the master array for the project based upon the user's criteria 
+    
+    var concatRepo = [];
+
+    if (upperCase ==true) {
+        concatRepo = concatRepo.concat(uc);
+        console.log(concatRepo)
+    }
+
+    if (lowerCase == true) {
+        concatRepo = concatRepo.concat(lc)
+        console.log(concatRepo)
+    }
+
+    if (numbers == true) {
+        concatRepo = concatRepo.concat(num);
+        console.log(concatRepo)
+    }
+    if (specialCharacters == true) {
+        concatRepo = concatRepo.concat(sc)
+        console.log (concatRepo)
+    }
+    
+    console.log ("The final array includes: " + concatRepo)
+
+    // concatLength will give us the total array size for all the selected arrays.
+    
+    let concatLenghth = concatRepo.length
+    console.log ("The total length of the master array is : " + concatLenghth)
+
+// The following function should generate random numbers that will eventually relate to characters in the concatRepo and will generate our random password.
+
+function randomSelector(concatRepo) {
+    for (let i=0; i < length; i++) { 
+        return concatRepo = Math.floor(math.random() * concatLenghth)
+    }
+    console.log(randomSelector(concatRepo))
+}
+randomSelector()
+
+
+
+
+
+
+
+
+
+
  // PsuedoCode
 
   // function to validate user answrs
@@ -83,7 +130,7 @@ generateBtn.addEventListener("click", writePassword);
 
   //  } 
 
-    // combine all arrays in 1 array
+    
     // Randomly choose characters
     // create a string set equal to password var
     // array method to check if something is in array
@@ -119,3 +166,46 @@ generateBtn.addEventListener("click", writePassword);
 
 //   return password;
 // }
+
+// **** Graveyard of unused code **** //
+   // The following section contains variables to store the answers from the prompt section. Again the console.logs should be commented out prior to submitting the project//
+
+    // var length = pwdLength
+    // console.log ('The password length should be ' + length)
+
+    // var lCase = lowerCase
+    // console.log ('Should the password have lower case letters? : ' + lCase)
+
+    // var uCase= upperCase
+    // console.log ('Should the password have upper case letters? : ' + uCase)
+
+    // var number = numbers
+    // console.log ('Should there be numbers in the password? : ' + number)
+
+    // var specialChar = specialCharacters
+    // console.log ('Should the password have special characters? : ' + specialChar)
+
+    
+    
+        // uc = uc.concat(lc, num)
+        // console.log(uc)
+
+    // if (upperCase == true) {
+    //     concatRepo.push.apply (concatRepo, uc);
+    //     console.log(concatRepo);
+    // }
+    
+    // if (lowerCase == true){
+    //     concatRepo.push.apply (concatRepo, lc);
+    //     // console.log (concatRepo)
+    // }
+
+    // if (numbers == true) {
+    //     concatRepo.push.apply (concatRepo, num)
+    //     // console.log (concatRepo)
+    // }
+    
+    // if (specialCharacters == true) {
+    //     concatRepo.push.apply (concatRepo, sc);
+    //     // console.log (concatRepo)
+    // }
